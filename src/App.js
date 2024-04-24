a55c6fe31b4741a92bae4b45b982939597e23dd7
Marwa Babelly <marwaababelly@gmail.com>
2024-04-24 17:48:23 +0300

    add font and basic color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import wool from "../src/assets/wool1.webp";
+import logo from "../src/assets/logo.jpg";
 
 function App() {
   return (
     <div className="App-header">
+      <img alt="logo" src={logo} />
       <p>Hi React</p>
+      <img alt="woolImg" src={wool} />
     </div>
   );
 }
