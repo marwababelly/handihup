d15d9516812366c95f1a53a22fe4f309d396d692
marwababelly <marwaababelly@gmail.com>
2024-06-02 20:29:31 +0300

    Add modifications to the product page details

--- src/Pages/Projects/ProjectDetails.js
+++ src/Pages/Projects/ProjectDetails.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
-import { Carousel } from "react-bootstrap";
+import { Carousel, Form, Button } from "react-bootstrap";
 import style from "./ProjectDetails.module.css";
 import fontArt from "../../assets/fontArt4.jpg";
 import skinCare from "../../assets/skinCare.jpg";
@@ -12,7 +12,7 @@ const ProjectDetails = () => {
     {
       id: 1,
       dProjectName: "Pottery",
-      dProjectImg: pottery,
+      dProjectImg: [pottery, skinCare, fontArt, rings],
       dProjectDescription:
         "This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on or double click on the text box to start This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on or double click on the text box to start.",
       dProjectContents: ["pottery", "pen", "ink", "clay"],
@@ -21,11 +21,12 @@ const ProjectDetails = () => {
         "Nulla vitae elit libero, a pharetra augue mollis interdum Null vitae elit libero, a pharetra augue mollis interdum Nulla vitae elitlibero, a pharetra augue mollis interdum Nulla vitae elit libero, apharetra augue mollis interdum.",
       ],
       link: "Pottery",
+      dprice: "500$",
     },
     {
       id: 2,
       dProjectName: "SkinCare",
-      dProjectImg: skinCare,
+      dProjectImg: [pottery, skinCare, fontArt, rings],
       dProjectDescription:
         "This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on or double click on the text box to start This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on or double click on the text box to start.",
       dProjectContents: ["pottery", "pen", "ink", "clay"],
@@ -34,11 +35,12 @@ const ProjectDetails = () => {
         "Nulla vitae elit libero, a pharetra augue mollis interdum Null vitae elit libero, a pharetra augue mollis interdum Nulla vitae elitlibero, a pharetra augue mollis interdum Nulla vitae elit libero, apharetra augue mollis interdum.",
       ],
       link: "SkinCare",
+      dprice: "500$",
     },
     {
       id: 3,
       dProjectName: "FontArt",
-      dProjectImg: fontArt,
+      dProjectImg: [pottery, skinCare, fontArt, rings],
       dProjectDescription:
         "This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on or double click on the text box to start This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on or double click on the text box to start.",
       dProjectContents: ["pottery", "pen", "ink", "clay"],
@@ -47,11 +49,12 @@ const ProjectDetails = () => {
         "Nulla vitae elit libero, a pharetra augue mollis interdum Null vitae elit libero, a pharetra augue mollis interdum Nulla vitae elitlibero, a pharetra augue mollis interdum Nulla vitae elit libero, apharetra augue mollis interdum.",
       ],
       link: "FontArt",
+      dprice: "500$",
     },
     {
       id: 4,
       dProjectName: "Accessories",
-      dProjectImg: rings,
+      dProjectImg: [pottery, skinCare, fontArt, rings],
       dProjectDescription:
         "This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on or double click on the text box to start This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on or double click on the text box to start.",
       dProjectContents: ["pottery", "pen", "ink", "clay"],
@@ -60,10 +63,41 @@ const ProjectDetails = () => {
         "Nulla vitae elit libero, a pharetra augue mollis interdum Null vitae elit libero, a pharetra augue mollis interdum Nulla vitae elitlibero, a pharetra augue mollis interdum Nulla vitae elit libero, apharetra augue mollis interdum.",
       ],
       link: "Accessories",
+      dprice: "500$",
     },
   ];
 
   const [index, setIndex] = useState(0);
+  const [reviews, setReviews] = useState([]);
+  const [report, setReport] = useState();
+  const reviewRef = useRef(null);
+  const reportRef = useRef(null);
+
+  const handleAddReview = (event) => {
+    event.preventDefault();
+    const newReview = reviewRef.current.value.trim();
+    if (newReview) {
+      setReviews([...reviews, newReview]);
+      reviewRef.current.value = "";
+    }
+  };
+
+  const handleReport = (e) => {
+    setReport(e.target.value);
+  };
+
+  const handleSubmitReport = (e) => {
+    e.preventDefault();
+    alert("Form submitted successfully!");
+    reportRef.current.value = "";
+  };
+
+  const reviewSlides = reviews.map((review, index) => (
+    <Carousel.Item key={index} className={style.carouselItem}>
+      <p className={style.par}>{review}</p>
+    </Carousel.Item>
+  ));
+
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
@@ -79,14 +113,40 @@ const ProjectDetails = () => {
     <div className={style.allContent}>
       <div className={style.container}>
         <div className={style.backgroundContainer}>
-          <h2>{productsDetailPage.dProjectName}</h2>
-          <img src={productsDetailPage.dProjectImg} alt="product Img" />
+          <h2 className="fw-bold mb-2 text-center text-uppercase text-secondary">
+            {productsDetailPage.dProjectName}
+          </h2>
+          <Carousel
+            className={style.carouselImg}
+            activeIndex={index}
+            variant="dark"
+            interval={2500}
+            onSelect={handleSelect}
+          >
+            {" "}
+            {productsDetailPage.dProjectImg.length > 0 &&
+              productsDetailPage.dProjectImg.map((img, index) => (
+                <Carousel.Item className={style.carouselItemImg}>
+                  <img
+                    className={style.img}
+                    key={index}
+                    src={img}
+                    alt="alt img"
+                  />
+                </Carousel.Item>
+              ))}
+          </Carousel>
+          <p className={style.price}>
+            The price per piece is: {productsDetailPage.dprice}
+          </p>
         </div>
         <p className={style.description}>
           {productsDetailPage.dProjectDescription}
         </p>
         <div className={style.contentDiv}>
-          <h3>project contents:</h3>
+          <h3 className="fw-bold mb-2 text-center text-uppercase text-secondary mt-3">
+            project contents:
+          </h3>
           <div className={style.content}>
             {productsDetailPage.dProjectContents.map((detail, detailIndex) => (
               <div className={style.element}>
@@ -101,6 +161,9 @@ const ProjectDetails = () => {
             ))}
           </div>
         </div>
+        <h3 className="fw-bold mb-2 text-center text-uppercase text-secondary mt-3">
+          Reviews
+        </h3>
         <Carousel
           className={style.carousel}
           activeIndex={index}
@@ -108,12 +171,58 @@ const ProjectDetails = () => {
           interval={2500}
           onSelect={handleSelect}
         >
-          {productsDetailPage.dProjectReviews.map((review) => (
-            <Carousel.Item className={style.carouselItem}>
-              <p>{review}</p>
-            </Carousel.Item>
-          ))}
+          {productsDetailPage.dProjectReviews.length > 0 &&
+            productsDetailPage.dProjectReviews.map((review, index) => (
+              <Carousel.Item className={style.carouselItem}>
+                <p key={index} className={style.par}>
+                  {review}
+                </p>
+              </Carousel.Item>
+            ))}
+          {reviewSlides}
         </Carousel>
+
+        <Form onSubmit={handleAddReview}>
+          <Form.Group className="mb-3" controlId="addReview">
+            <Form.Label className={style.label}>Add Review</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              type="description"
+              className={style.customInput}
+              ref={reviewRef}
+              required
+            />
+          </Form.Group>
+          <div className={style.divBtn}>
+            <Button className={style.button} type="submit">
+              Add
+            </Button>
+          </div>
+        </Form>
+
+        <div className={style.reportSession}>
+          <h5 className="fw-bold mb-2 text-center text-uppercase text-secondary mt-3">
+            If anything happens that is illegal or not beneficial to the site's
+            desires, please report it{" "}
+          </h5>
+          <Form onSubmit={handleSubmitReport} className={style.reportForm}>
+            <Form.Group className="mb-3" controlId="addReport">
+              {/* <Form.Label className={style.label}>Add Report</Form.Label> */}
+              <Form.Control
+                type="text"
+                className={style.customInput}
+                // value={report}
+                onChange={handleReport}
+                ref={reportRef}
+                required
+              />
+            </Form.Group>
+            <Button className={style.button} type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
     </div>
   );
