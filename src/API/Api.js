be0cbb8d2abf812933d2397d1f72ef82e2189164
AmalMakki <amalmakki.2.9.2002@gmail.com>
2024-08-12 00:59:24 +0300

    axios interceptor

--- src/API/Api.js
+++ src/API/Api.js
@@ -2,7 +2,7 @@ export const baseURL = "http://127.0.0.1:8000/api";
 
 export const LogIn = "login";
 
-export const SignUp = "signup";
+export const SignUp = "user";
 
 export const Projects = "projects";
 
--- src/API/axios.js
+++ src/API/axios.js
@@ -1,6 +1,48 @@
-import axios from "axios";
-import { baseURL } from "./Api";
+import axios from 'axios';
 
-export const Axios = axios.create({
-  baseURL: baseURL,
+const apiClient = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
 });
+
+apiClient.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async error => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem('refreshToken'); 
+      if (refreshToken) {
+        try {
+          const response = await apiClient.post('/refresh_token', { refreshToken });
+          localStorage.setItem('token', response.data.accessToken);
+          return apiClient(originalRequest);
+        } catch (err) {
+          localStorage.removeItem('token');
+          localStorage.removeItem('refreshToken');
+          window.location.href = '/login'; 
+        }
+      } else {
+        localStorage.removeItem('token');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/login'; 
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
+
